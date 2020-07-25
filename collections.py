c1422dca669d176ab41e3b3a4ef3e97b197121af
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-07-25 10:45:38 -0500

    list examples

---
+++ collections.py
@@ -0,0 +1,22 @@
+# Collections examples
+
+# List
+my_colors = ["red", "blue", "orange", "yellow"]
+
+print("\n", my_colors)
+
+# add vr insert
+my_colors.append("purple")
+my_colors.insert(3, "brown")
+
+print("\n", my_colors)
+
+# remove
+my_colors.remove("brown")
+my_colors.pop()
+del my_colors[1]
+
+print("\n", my_colors)
+
+
+# List Slice
